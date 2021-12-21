ba48318ceea75904932ddef7dc9940d04022b44e
Nikolay Kolesnikov <zveroboy85@gmail.com>
2021-12-21 18:25:03 +0300

    Improved #217

--- ts/src/00217_Contains_Duplicate/index.ts
+++ ts/src/00217_Contains_Duplicate/index.ts
@@ -1,9 +1,14 @@
-export function containsDuplicate(nums: number[]): boolean {
+export function containsDuplicateSimple(nums: number[]): boolean {
   nums.sort()
-  for(let i=1; i < nums.length; i++){
-      if(nums[i] === nums[i-1]){
-          return true
-      }
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) {
+      return true
+    }
   }
   return false
+}
+
+export function containsDuplicate(nums: number[]): boolean {
+  const set = new Set<number>(nums)
+  return set.size !== nums.length
 }
