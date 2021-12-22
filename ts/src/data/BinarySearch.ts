7add2b9163062f708b0e441fff8c9ec949747658
Nikolay Kolesnikov <zveroboy85@gmail.com>
2021-12-22 17:53:47 +0300

    Added new Binary searches

--- ts/src/data/BinarySearch.ts
+++ ts/src/data/BinarySearch.ts
@@ -19,3 +19,43 @@ export const binarySearch = function* (
     }
   }
 }
+
+export const binarySearchRight = function* (
+  max: number,
+  pred: Predicate,
+): Generator<number> {
+  if (max < 1) return
+  let from = 0,
+    to = max
+  while (from < to) {
+    const mid = from + Math.floor((to - from) / 2)
+
+    yield mid
+
+    if (pred(from, mid, to)) {
+      to = mid
+    } else {
+      from = mid + 1
+    }
+  }
+}
+
+export const binarySearchLeftRight = function* (
+  max: number,
+  pred: Predicate,
+): Generator<number> {
+  if (max < 1) return
+  let from = -1,
+    to = max
+  while (from + 1 < to) {
+    const mid = from + Math.floor((to - from) / 2)
+
+    yield mid
+
+    if (pred(from, mid, to)) {
+      to = mid
+    } else {
+      from = mid
+    }
+  }
+}
