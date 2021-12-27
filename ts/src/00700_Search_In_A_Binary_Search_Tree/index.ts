98d3fcac0d543779696168765ce7810830bead7e
Nikolay Kolesnikov <zveroboy85@gmail.com>
2021-12-27 16:15:34 +0300

    Improved BST

--- ts/src/00700_Search_In_A_Binary_Search_Tree/index.ts
+++ ts/src/00700_Search_In_A_Binary_Search_Tree/index.ts
@@ -1,13 +1,17 @@
 import { TreeNode } from 'data/TreeNode'
 
 export function searchBST(root: TreeNode | null, val: number): TreeNode | null {
-  if (root?.val === val) {
-    return root
-  }
-
-  if (!root?.left && !root?.right) {
+  if (!root) {
     return null
   }
 
-  return searchBST(root?.left, val) || searchBST(root?.right, val)
+  if (root.val > val) {
+    return searchBST(root.left, val)
+  }
+
+  if (root.val < val) {
+    return searchBST(root.right, val)
+  }
+
+  return root
 }
